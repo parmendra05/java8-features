ea6475b331c4ab120e82f570baa0724796afc9dc
parmendra kumar <parmendrakumar718@gmail.com>
2023-08-25 15:47:39 +0530

    lambda

--- src/lambda/Test_1.java
+++ src/lambda/Test_1.java
@@ -2,5 +2,36 @@ package lambda;
 
 public class Test_1 {
 
-	
+	public static void main(String[] args) {
+		// Before java 8 Without Lambda
+		Oppo oppo = new Oppo();
+		oppo.greet();
+
+		// without lambda with anonymous inner type
+		Mobile samsung = new Mobile() {
+
+			@Override
+			public void greet() {
+				System.out.println("Welcome to Samsung mobile, anonymous function");
+
+			}
+		};
+		samsung.greet();
+
+		// With Lambda
+		Mobile vivo = () -> System.out.println("Welcome to Vivo Mobile, with Lambda");
+		vivo.greet();
+	}
+}
+
+interface Mobile {
+	public void greet();
+}
+
+class Oppo implements Mobile {
+
+	public void greet() {
+
+		System.out.println("Welcome to Oppo Mobile");
+	}
 }
